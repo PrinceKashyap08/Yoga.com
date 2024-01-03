290004d9908800507fbdd17a0809b0c508b25d5c
Prince <vasukashyap08@gmail.com>
2024-01-03 15:59:30 +0530

    commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$('#homeHero .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 40,
+    nav: false,
+    autoplay: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
